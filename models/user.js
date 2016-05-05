70f4186d9969dd3ebde189ad3a9f8f8f4e1b0fa8
Juan Sanchez <juan.sanchez1192@gmail.com>
2016-05-05 11:22:41 -0430

    Add User model with statics and instance methods

--- models/user.js
+++ models/user.js
@@ -1,8 +1,35 @@
+/**
+ * @author Juan Sanchez
+ * @description User model definition
+ * @lastModifiedBy Juan Sanchez
+ */
+
 'use strict';
 
+/**
+ * Module dependencies.
+ */
+
 const mongoose = require('mongoose');
+const validate = require('mongoose-validator');
+const crypto = require('crypto');
 
 const Schema = mongoose.Schema;
+
+/**
+ * Validators
+ */
+
+const emailValidator = [
+  validate({
+    validator: 'isEmail',
+    message: 'not a valid email'
+  })
+];
+
+/**
+ * Schema
+ */
 
 const UserSchema = new Schema({
   firstName: {
@@ -24,12 +51,64 @@ const UserSchema = new Schema({
     type: Number,
     min: [0, '`{VALUE}` is not a valid age']
   },
-  email: {},
-  password: {},
-  authToken: {},
-
+  email: {
+    type: String,
+    required: true,
+    validate: emailValidator
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  authToken: {
+    type: String,
+    required: true
+  }
 }, {
   timestamps: true
 });
+
+/**
+ * Methods
+ */
+
+UserSchema.methods = {
+
+  /**
+   * Authenticate
+   *
+   * @param {String} plainText
+   * @return {Boolean}
+   * @description check if the passwords are the same
+   */
+
+  authenticate: function (plainText) {
+    return crypto.createHash('md5').update(plainText).digest('hex') === this.password;
+  }
+
+};
+
+/**
+* Statics
+*/
+
+UserSchema.statics = {
+
+  /**
+   * Load
+   *
+   * @param {Object} options
+   * @param {Function} cb
+   * @description get an user given a query criteria
+   */
+
+  load: function (options, cb) {
+    options.select = options.select || 'firstName lastName email';
+    return this.findOne(options.criteria)
+    .select(options.select)
+    .exec(cb);
+  }
+
+};
 
 const User = mongoose.model('User', UserSchema);
