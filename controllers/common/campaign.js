5b236de91241e9a2c84e62bd19557bacb772c127
Juan Sanchez <juan.sanchez1192@gmail.com>
2016-05-19 16:02:13 -0430

    Add Campaign CRUD, some validations need to be done

--- controllers/common/campaign.js
+++ controllers/common/campaign.js
@@ -14,38 +14,139 @@ const Campaign = mongoose.model('Campaign');
 
 module.exports = {
 
-  readAll: function (req, res) {
+  readAll(req, res) {
+    const locals = req.app.locals;
 
-    Campaign.paginate({
-      limit: req.query.limit,
-      offset: req.query.offset,
-      criteria: req.query.criteria
-    }, function (err, json) {
+    const criteria = req.query.criteria || {};
+    const offset = locals.config.offset(req.query.offset);
+    const limit = locals.config.limit(req.query.limit);
 
-      if (err)
-        return res.status(500).send(err);
+    Campaign.paginate(criteria, {
+      sort: {
+        createdAt: 1,
+      },
+      offset,
+      limit,
+      populate: ['companyId', 'games.gameId', 'games.missions.missionId'],
+    })
+    .then(campaigns => res.json(campaigns))
+    .catch(err => res.status(500).send(err));
+  },
 
-      res.json(json).end();
+  createByACompany(req, res) {
+    const criteria = Object.assign({ companyId: req.params.company_id }, req.body);
+
+    Campaign.create(criteria)
+    .then(campaign => res.status(201).json(campaign))
+    .catch(err => {
+      if (err.name === 'ValidationError' || err.name === 'InvalidDateRange')
+        return res.status(400).json(err).end();
+
+      return res.status(500).send(err);
     });
   },
 
-  createByACompany: function (req, res) {
+  readByACompany(req, res) {
+    const locals = req.app.locals;
 
+    if (req.query.active === 'true') {
+      const today = new Date();
+
+      Campaign.findOne({ startAt: { $lt: today }, finishAt: { $gte: today } })
+      .populate('companyId games.gameId games.missions.missionId')
+      .then(campaign => {
+        if (!campaign)
+          return res.status(404).end();
+        res.json(campaign);
+      })
+      .catch(err => {
+        if (err.name === 'CastError') {
+          return res.status(400).send(err);
+        }
+        return res.status(500).send(err);
+      });
+    } else {
+      const companyId = req.params.company_id;
+      const offset = locals.config.offset(req.query.offset);
+      const limit = locals.config.limit(req.query.limit);
+
+      Campaign.paginate({
+        companyId,
+      }, {
+        sort: {
+          createdAt: 1,
+        },
+        offset,
+        limit,
+        populate: ['companyId', 'games.gameId', 'games.missions.missionId'],
+      })
+      .then(campaigns => res.json(campaigns))
+      .catch(err => res.status(500).send(err));
+    }
   },
 
-  readByACompany: function (req, res) {
+  read(req, res) {
+    const criteria = {
+      companyId: req.params.company_id,
+      _id: req.params.campaign_id,
+    };
 
+    Campaign.findOne(criteria)
+    .populate('companyId games.gameId games.missions.missionId')
+    .then(campaign => {
+      if (!campaign)
+        return res.status(404).end();
+      res.json(campaign);
+    })
+    .catch(err => {
+      if (err.name === 'CastError') {
+        return res.status(400).send(err);
+      }
+      return res.status(500).send(err);
+    });
   },
 
-  read: function (req, res) {
+  update(req, res) {
+    const criteria = {
+      companyId: req.params.company_id,
+      _id: req.params.campaign_id,
+    };
 
+    Campaign.findOneAndUpdate(criteria, req.body, {
+      runValidators: true,
+      context: 'query',
+    })
+    .then(campaign => {
+      if (!campaign)
+        return res.status(404).end();
+      res.status(204).end();
+    })
+    .catch(err => {
+      if (err.name === 'CastError' || err.name === 'ValidationError') {
+        return res.status(400).send(err);
+      }
+      return res.status(500).send(err);
+    });
   },
 
-  update: function (req, res) {
+  delete(req, res) {
+    const criteria = {
+      companyId: req.params.company_id,
+      _id: req.params.campaign_id,
+    };
 
-  },
+    Campaign.findOneAndRemove(criteria)
+    .then(campaign => {
+      if (!campaign)
+        return res.status(404).end();
 
-  delete: function (req, res) {
-
+      res.status(204).end();
+    })
+    .catch(err => {
+      if (err.name === 'CastError') {
+        return res.status(400).send(err);
+      }
+      return res.status(500).send(err);
+    });
   },
 };
--- models/common/campaign.js
+++ models/common/campaign.js
@@ -7,6 +7,8 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
+const idValidator = require('mongoose-id-validator');
 
 const Schema = mongoose.Schema;
 
@@ -14,95 +16,88 @@ const MissionsListSchema = new Schema({
   missionId: {
     type: Schema.Types.ObjectId,
     required: true,
-    ref: 'Mission'
+    ref: 'Mission',
   },
   isRequired: {
     type: Boolean,
-    defualt: false
+    defualt: false,
   },
   isBlocking: {
     type: Boolean,
-    defualt: false
+    defualt: false,
   },
   blockedTime: { // Minutes
     type: Number,
-    default: 0
+    default: 0,
   },
   max: {
     type: Number,
     min: [1, '`{VALUE}` is not a valid max'],
-    default: 1
-  }
+    default: 1,
+  },
 }, { _id: false });
 
 const GamesListSchema = new Schema({
   gameId: {
     type: Schema.Types.ObjectId,
     required: true,
-    ref: 'Game'
+    ref: 'Game',
   },
-  missions: [MissionsListSchema]
+  missions: [MissionsListSchema],
 }, { _id: false });
 
 const CampaignSchema = new Schema({
   companyId: {
     type: Schema.Types.ObjectId,
     required: true,
-    ref: 'Company'
+    ref: 'Company',
   },
   games: [GamesListSchema],
   name: {
     type: String,
-    required: true
+    required: true,
   },
   banner: {
     type: String,
-    required: true
+    required: true,
   },
   startAt: {
     type: Date,
-    required: true
+    required: true,
   },
   finishAt: {
     type: Date,
-    required: true
-  }
+    required: true,
+  },
 }, {
-  timestamps: true
+  timestamps: true,
 });
 
 CampaignSchema.methods = {};
 
-CampaignSchema.statics = {
+CampaignSchema.statics = {};
 
-  paginate: function (options, cb) {
-    const model = this;
-    const criteria = options.criteria || {};
-    const offset = options.offset ? parseInt(options.offset) : 0;
-    const limit = options.limit ? parseInt(options.limit) : 20;
-    return model.find(criteria)
-    .sort({ createdAt: 1 })
-    .limit(limit)
-    .skip(offset)
-    .exec(function(err, data) {
-
-      if(err)
-        return cb(err);
-
-      return model.count(criteria)
-      .exec(function(err, total) {
-
-        if(err)
-          return cb(err);
-
-        return cb(null, { data, total, limit, offset });
-      });
-    });
-  }
-};
-
-CampaignSchema.pre('remove', function (next) {
+CampaignSchema.pre('remove', next => {
   next();
 });
 
-mongoose.model('Campaign', CampaignSchema);
+CampaignSchema.plugin(mongoosePaginate);
+CampaignSchema.plugin(idValidator);
+
+const Campaign = mongoose.model('Campaign', CampaignSchema);
+
+CampaignSchema.pre('save', function (next) {
+  Campaign.find({
+    $or: [{ startAt: { $lte: this.startAt }, finishAt: { $gte: this.startAt } },
+    { startAt: { $lte: this.finishAt }, finishAt: { $gte: this.finishAt } }],
+  })
+  .then(campaigns => {
+    if (campaigns.length > 0) {
+      const err = new Error('InvalidDateRange');
+      err.name = 'InvalidDateRange';
+      return next(err);
+    }
+    next();
+  })
+  .catch(next);
+});
