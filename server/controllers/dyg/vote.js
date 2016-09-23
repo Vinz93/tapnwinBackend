4da6f43952df2efd600d1dfc603e3888607317e7
Carlos Avilan <carlosa@ludopia.net>
2016-09-23 16:45:43 -0400

    Update CampaignStatus of owner after every vote

--- server/controllers/dyg/vote.js
+++ server/controllers/dyg/vote.js
@@ -1,15 +1,19 @@
 /**
  * @author Andres Alvarez
- * @description Company controller definition
- * @lastModifiedBy Andres Alvarez
+ * @description Vote controller definition
+ * @lastModifiedBy Carlos Avilan
  */
 import httpStatus from 'http-status';
+import timeUnit from 'time-unit';
 import Promise from 'bluebird';
 
 import { paginate } from '../../helpers/utils';
 import APIError from '../../helpers/api_error';
 import Design from '../../models/dyg/design';
 import Vote from '../../models/dyg/vote';
+import MissionStatus from '../../models/common/mission_status';
+import MissionCampaign from '../../models/common/mission_campaign';
+import CampaignStatus from '../../models/common/campaign_status';
 
 const VoteController = {
 /**
@@ -115,7 +119,161 @@ const VoteController = {
     req.body.player = res.locals.user._id;
 
     Vote.create(req.body)
-    .then(vote => res.status(httpStatus.CREATED).json(vote))
+    .then(vote => [
+      vote,
+      Vote.populate(vote, 'design'),
+    ])
+    .spread((vote, votePopulated) => [
+      vote,
+      votePopulated,
+      CampaignStatus.findOrCreate({
+        player: votePopulated.design.player,
+        campaign: votePopulated.design.campaign,
+      }),
+    ])
+    .spread((vote, votePopulated, campaignStatus) => [
+      vote,
+      votePopulated,
+      CampaignStatus.populate(campaignStatus, 'campaign'),
+    ])
+    .spread((vote, votePopulated, campaignStatus) => [
+      vote,
+      votePopulated,
+      campaignStatus,
+      MissionCampaign.find({
+        campaign: campaignStatus.campaign._id,
+      }),
+    ])
+    .spread((vote, votePopulated, campaignStatus, missionCampaigns) => [
+      vote,
+      votePopulated,
+      campaignStatus,
+      Promise.map(missionCampaigns, missionCampaign =>
+        MissionStatus.findOne({
+          player: votePopulated.design.player,
+          missionCampaign: missionCampaign.id,
+        })
+        .populate({
+          path: 'missionCampaign',
+          model: 'MissionCampaign',
+          populate: [
+            {
+              path: 'mission',
+              model: 'Mission',
+            },
+          ],
+        })
+        .then(missionStatus => {
+          if (!missionStatus) {
+            return MissionStatus.create({
+              player: votePopulated.design.player,
+              missionCampaign: missionCampaign.id,
+            })
+            .then(missionStatus => MissionStatus.populate(missionStatus, {
+              path: 'missionCampaign',
+              model: 'MissionCampaign',
+              populate: [
+                {
+                  path: 'mission',
+                  model: 'Mission',
+                },
+              ],
+            }));
+          }
+          return missionStatus;
+        })
+      ),
+    ])
+    .spread((vote, votePopulated, campaignStatus, missionStatuses) => [
+      vote,
+      votePopulated,
+      campaignStatus,
+      missionStatuses,
+      Vote.aggregate([
+        {
+          $group: {
+            _id: {
+              design: '$design',
+            },
+            count: { $sum: 1 },
+          },
+        },
+        { $sort: { count: 1 } },
+      ], (err, votes) =>
+        Design.find({ _id: { $in: votes.map(item => item._id.design) } })
+        .then(designs => {
+          const designsVoted = designs.map((item, index) => {
+            const designAndCount = {};
+            designAndCount.count = votes[index].count;
+            designAndCount.design = item;
+            // console.log(designAndCount);
+            return designAndCount;
+          })
+          .filter(item =>
+            item.design.player.toString() === votePopulated.design.player.toString()
+          );
+          return designsVoted.reduce((prev, item) =>
+            Math.max(prev, item.count), 0);
+        })
+        .then(votes => {
+          campaignStatus.dyg.votesReceived = votes;
+          return campaignStatus.save();
+        })
+        .then(campaignStatus => {
+          let blocked = false;
+          const missionsUpdated = missionStatuses.map(item => {
+            const code = item.missionCampaign.mission.code;
+            if (code === '0105' && !item.isDone) {
+              // Tener (N) votos en uno de los diseños propios
+              if (campaignStatus.dyg.votesReceived >= item.missionCampaign.max) {
+                // Mission Completed
+                if (item.missionCampaign.isBlocking) {
+                  const blockTime = new Date(Date.now() +
+                    timeUnit.hours.toMillis(item.missionCampaign.blockTime));
+                  if (campaignStatus.campaign.m3.blockable) {
+                    campaignStatus.m3.isBlocked = true;
+                    campaignStatus.m3.unblockAt = blockTime;
+                  }
+                  if (campaignStatus.campaign.dyg.blockable) {
+                    campaignStatus.dyg.isBlocked = true;
+                    campaignStatus.dyg.unblockAt = blockTime;
+                  }
+                  if (campaignStatus.campaign.vdlg.blockable) {
+                    campaignStatus.vdlg.isBlocked = true;
+                    campaignStatus.vdlg.unblockAt = blockTime;
+                  }
+                }
+                item.value = item.missionCampaign.max;
+                item.isDone = true;
+              } else {
+                // Mission updated
+                item.value = campaignStatus.dyg.votesReceived;
+              }
+            }
+            if (!item.isDone && item.missionCampaign.isRequired) {
+              blocked = true;
+            }
+            return item;
+          });
+
+          campaignStatus.isBlocked = blocked;
+          // console.log(campaignStatus);
+          return [campaignStatus.save(), missionsUpdated];
+        })
+        .spread((campaignStatus, missionsUpdated) =>
+          Promise.map(missionsUpdated, missionUpdated =>
+            MissionStatus.findOne({
+              _id: missionUpdated.id,
+            })
+            .then(mission => {
+              mission.set(missionUpdated);
+              return mission.save();
+            })
+          )
+        )
+        .then(() => res.status(httpStatus.CREATED).json(vote))
+      ),
+    ])
     .catch(next);
   },
 
