3743c947754f5c031c530c62da2016fb465f59e7
vincenzo <vincenzobianco1993@gmail.com>
2016-09-30 15:33:49 -0400

    fix ident issues

--- server/helpers/providers_login.js
+++ server/helpers/providers_login.js
@@ -1,16 +1,16 @@
 const createAndLogin = (res, status, player) => {
-    if (player.verified == false)
-        player.verified = true;
-    if (player.verificationToken)
-        player.verificationToken = undefined;
+  if (player.verified === false)
+    player.verified = true;
+  if (player.verificationToken)
+    player.verificationToken = undefined;
 
-    player.createSessionToken();
-    player.lastLogin = Date.now();
-    player.save()
-        .then(player => {
-            res.status(status).json(player);
-        })
-        .catch(err => res.json(err));
+  player.createSessionToken();
+  player.lastLogin = Date.now();
+  player.save()
+    .then(player => {
+      res.status(status).json(player);
+    })
+    .catch(err => res.json(err));
 };
 
 export default createAndLogin;
