b1ddaca023b264c1d5a1e3bf8a40dbff1771f937
Vincenzo Bianco <vincenzob@ludopia.net>
2016-11-09 17:39:57 -0400

    keep with the test structure

--- server/tests/player.js
+++ server/tests/player.js
@@ -1,16 +1,19 @@
 import assert from 'assert';
-import request from 'super-agent';
+// import { assert } from 'chai';
+import request from 'superagent';
 import mongoose from 'mongoose';
-import config from '../config/env';
-import Player from '../models/common/Player';
-import User from '../models/common/User';
-import app from '../config/express';
+import Promise from 'bluebird';
+import config from '../../config/env';
+// import Player from '../models/common/player';
+import User from '../models/common/user';
+import app from '../../config/express';
 
-const API_URL = 'localhost:3003';
+const API_URL = 'localhost:3004';
+mongoose.Promise = Promise;
 
 describe('User Tests', () => { // eslint-disable-line
 
-  it('Creates a user', (done) => { // eslint-disable-line
+  it('Creates a user', done => { // eslint-disable-line
     request
     .post(`${API_URL}/players`)
     .send({
@@ -24,12 +27,26 @@ describe('User Tests', () => { // eslint-disable-line
       const user = JSON.parse(res.text);
       assert.equal(user.email, 'vincenzob@ludopia.net');
       assert.equal(user.verified, false);
+      done();
     });
   });
 
+  it('the token has 4 digits', done => { // eslint-disable-line
+    User.findOne({ email: 'vincenzob@ludopia.net' })
+      .then(user => {
+        const token = Number(user.verificationToken);
+        assert.equal(typeof token, 'number');
+        assert.equal(user.verificationToken.length, 4);
+        done();
+      });
+  });
+
   before(() => { // eslint-disable-line
-    User.remove({ email: 'vincenzob@ludopia.net' });
     mongoose.connect(config.db);
-    app.listen(3003);
+    app.listen(3004);
+    User.remove({ email: 'vincenzob@ludopia.net' }, err => {
+      if (err)
+        console.log('error deleting vinz');
+    });
   });
 });
